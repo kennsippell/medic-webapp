a7f59a642e70ec03f53b85b6ae40ef64bdd8c260
Kenn Sippell <kennsippell@gmail.com>
2019-01-10 00:12:53 +0300

    test failure - swprecache expects /medic/ instead of /medic-test/

--- Gruntfile.js
+++ Gruntfile.js
@@ -291,7 +291,7 @@ module.exports = function(grunt) {
     },
     generateServiceWorker: {
       config: {
-        rootUrl: '/medic/_design/medic/_rewrite/',
+        rootUrl: `/${couchConfig.dbName}/_design/medic/_rewrite/`,
         staticDirectoryPath: 'build/ddocs/medic/_attachments',
         scriptOutputPath: 'build/ddocs/medic/_attachments/js/service-worker.js',
       }
